1e80f9a1eadcd168eb2442a4934bfbaf831f9fb8
Dixman001 <dixmaneagle@gmail.com>
2022-12-15 06:07:19 -0800

    task 5

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,10 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_leaf - checks if node is a leaf
+ * @node: node to check
  *
+ * Return: 1 if its a leaf, 0 if its not a leaf or NULL
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,17 @@
+#include "binary_tests.h"
+
+/**
+ * binary_tree_is_root - checks if node is a root
+ * @node: A pointer to the node to check
+ *
+ * Return: 1 if node is root or 0 if its not a root or NULL
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node->parent = NULL)
+		return (1);
+	return (0);
+
+	if (node = NULL)
+		return (0);
