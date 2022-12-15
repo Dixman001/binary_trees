751b22104b97f4153df657d1574940b4d02ebccc
Dixman001 <dixmaneagle@gmail.com>
2022-12-15 05:26:31 -0800

    task 3/4

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes the ebtire binary tree.
+ * @tree: a pointer to root node.
+ * 
+ * Description: if tree is null do nothing.
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free (tree);
+	}
+}
